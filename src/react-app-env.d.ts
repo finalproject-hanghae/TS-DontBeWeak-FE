4e7fb035618f0319288ca3d770eaa9e95500ea26
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-09-01 10:59:52 +0900

    Setting envs

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+//env 파일 typed를 통한 자동완성 가능화
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV : 'production' | 'development' ;
+        REACT_APP_DB_HOST : string;
+    }
+}
